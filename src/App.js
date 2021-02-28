7015ba870632ed88397086960c6d3f19896b9bef
Tuhin Khan <webwithjhankarvai@gmail.com>
2021-02-28 23:18:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[countries, setCountries] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data);
+    })
+    .catch (error => console.log(error))
+  },[])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Countries Loaded : {countries.length}</h2>
+      <ul>
+        {
+          countries.map( country => <li>{country.name}</li>)
+        }
+      </ul>
+   
     </div>
   );
 }
